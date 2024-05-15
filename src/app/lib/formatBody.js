d65d7215ec7fd15d6f1aaa30297f03418f49f501
Tauriel1202 <lothlorienexplorer20@gmail.com>
2024-05-15 00:35:15 -0400

    username fixed

--- src/app/lib/formatBody.js
+++ src/app/lib/formatBody.js
@@ -4,7 +4,17 @@ export default async function formatBody(req) {
   let searchParams = new URLSearchParams(body);
   let data = Array.from(searchParams.entries()).reduce(
     (result, [key, value]) => Object.assign(result, { [key]: value }),
-    {},
+    {}
   );
+
+  data["username"] = `${data["adjs"]}${data["objs"]}${data["animals"]}`;
+  delete data["adjs"];
+  delete data["objs"];
+  delete data["animals"];
+  
+  let keys = Object.keys(data);
+  if (keys[0].includes("$ACTION_ID")) {
+    delete data[keys[0]];
+  }
   return data;
 }
